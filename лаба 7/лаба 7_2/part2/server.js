de06055fdf5c9a0558a3fbd044781fcc590fb7b3
Julia Kanoplich <yulia.kanoplich@gmail.com>
2024-04-19 18:07:50 +0300

    add labs

---
+++ лаба 7/лаба 7_2/part2/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Указываем Express, что файлы в текущей директории статические
+app.use(express.static(path.join(__dirname)));
+
+// Устанавливаем маршрут для отдачи файла index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Устанавливаем маршрут для скачивания файла cafe.json
+app.get('/transport.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'cafe.json'));
+});
+
+// Запускаем сервер
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
